fc80869b484cdea2f99d695d973674b0da63ec7b
Tsubasa Takayama <t-takayama@feedforce.jp>
2021-06-23 11:12:02 +0900

    Upload aarch64 RPM files for latest releases

--- .circleci/github-release.sh
+++ .circleci/github-release.sh
@@ -4,11 +4,6 @@ set -xe
 
 RUBY_VERSION=$(grep '^Version: ' ruby-${1}.spec | awk '{ print $NF }')
 
-need_to_release() {
-	http_code=$(curl -sL -w "%{http_code}\\n" https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases/tag/${RUBY_VERSION} -o /dev/null)
-	test $http_code = "404"
-}
-
 get_github_release() {
   version=v0.7.5
   wget https://github.com/meterup/github-release/releases/download/${version}/linux-amd64-github-release.bz2
@@ -18,25 +13,8 @@ get_github_release() {
   mv linux-amd64-github-release $HOME/bin/github-release
 }
 
-if ! need_to_release; then
-	echo "$CIRCLE_PROJECT_REPONAME $RUBY_VERSION has already released."
-	exit 0
-fi
-
 get_github_release
 cp $CIRCLE_ARTIFACTS/*.rpm .
-
-#
-# Create a release page
-#
-
-$HOME/bin/github-release release \
-  --user $CIRCLE_PROJECT_USERNAME \
-  --repo $CIRCLE_PROJECT_REPONAME \
-  --tag $RUBY_VERSION \
-  --name "Ruby-${RUBY_VERSION}" \
-  --description "not release" \
-  --target master
 
 #
 # Upload rpm files and build a release note
@@ -67,9 +45,15 @@ Build on CentOS 7
 EOS
 
 # CentOS 7
+
+# Upload RPM files only aarch64
+for i in *.el7.centos.aarch64.rpm; do
+  upload_rpm $i
+done
+
+# Prepare to edit description
 for i in *.el7.centos.*.rpm; do
   print_rpm_markdown $i >> description.md
-  upload_rpm $i
 done
 
 #
